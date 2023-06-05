eaa53a4179eb01f3eee52241e5dce8c6c88d7252
Jacob Thornton <jacobreidth@hotmail.com>
2023-06-05 15:26:59 -0700

    Updated item.py for linting

--- Types/item.py
+++ Types/item.py
@@ -15,44 +15,26 @@ class Item:
         self.itemCost = int(itemCost)
         self.itemSpecialProperties = str(itemSpecialProperties)
         
-
     # Getters
     def getName(self):
         return self.itemName
-    
-    
     def getAttack(self):
         return self.itemAttack
-    
-    
     def getCost(self):
         return self.itemCost
-    
-    
     def getSpecialProperties(self):
         return self.itemSpecialProperties
-    
-    
     def getType(self):
         return self.itemType
-    
     
     # Setters
     def setName(self, data):
         self.itemName = data
-
-
     def setAttack(self, data):
         self.itemAttack = data
-
-    
     def setCost(self,data):
         self.itemCost = data
-
-    
     def setSpecialProperties(self,data):
         self.itemSpecialProperties = data
-        
-    
     def setType(self,data):
         self.itemType = data
