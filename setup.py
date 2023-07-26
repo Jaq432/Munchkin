bf267dfff9bc7be884fe95fd81e8cee8d2c20060
Jacob Thornton <jacobreidth@hotmail.com>
2023-07-26 12:54:23 -0700

    Updated gitignore and setup requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Munchkin",
-    version="0.1.0",
+    version="0.2.0",
     description="This is my discount version of the game Munchkin",
     author="Jacob Thornton",
     author_email="jacobreidth@hotmail.com",
@@ -10,9 +10,10 @@ setup(
     install_requires=[
         # List your project dependencies here
         "pytest==7.3.1",
+        "python==3.8.2"
     ],
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 2 - Beta",
         "Intended Audience :: Friends and Family",
         "License :: None?",
         "Programming Language :: Python :: 3.8.2",
